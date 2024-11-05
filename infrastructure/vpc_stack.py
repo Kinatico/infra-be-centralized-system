c1c3eb30c0f459f6b626832a2bb9806b6e4864ee
joe <ndson@lolconsultants.com>
2024-11-05 08:24:13 +0700

    build vpc infra

--- infrastructure/vpc_stack.py
+++ infrastructure/vpc_stack.py
@@ -1,6 +1,6 @@
 from aws_cdk import (
+    aws_ec2 as ec2,
     Stack,
-    aws_s3 as s3,
 )
 from constructs import Construct
 
@@ -10,4 +10,15 @@ class VPCStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, pr_number: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        s3.Bucket(self, "Bucketjoe143", bucket_name=f"bucketjoe143{pr_number}", versioned=True)
+        # Create a VPC has 1 Private Subnet and 0 NAT Gateway
+        vpc = ec2.Vpc(self, "biwoco-VPC",
+                      max_azs=2,
+                      nat_gateways=0,
+                      subnet_configuration=[
+                          ec2.SubnetConfiguration(
+                              name="PrivateSubnet",
+                              subnet_type=ec2.SubnetType.PRIVATE_ISOLATED,
+                              cidr_mask=24,
+                          )
+                      ]
+                      )
