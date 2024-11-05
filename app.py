917539e779d6b977665b6b49f94aea000424f0cd
joe <ndson@lolconsultants.com>
2024-11-05 12:12:21 +0700

    build ecs task get image from ecr

--- app.py
+++ app.py
@@ -9,10 +9,14 @@ from infrastructure.task_store_service_stack import TaskStoreServiceStack
 
 app = cdk.App()
 pr_number = os.getenv('PR_NUMBER') if os.getenv('PR_NUMBER') else ""
+environment = app.node.try_get_context("environments.staging")
+account = environment["account"]
+region = environment["region"]
+env = cdk.Environment(account=account, region=region)
 
 if not pr_number:
     VPCStack(app, f"VPCStack{pr_number}", pr_number=pr_number)
 
-TaskStoreServiceStack(app, f"TaskStoreServiceStack{pr_number}", pr_number=pr_number)
+TaskStoreServiceStack(app, f"TaskStoreServiceStack{pr_number}", pr_number=pr_number, env=env)
 
 app.synth()
