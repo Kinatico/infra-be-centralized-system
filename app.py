80f32873543a38b310e848f2cb58da47207c497e
joe <ndson@lolconsultants.com>
2024-11-05 12:19:23 +0700

    build ecs task get image from ecr

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import json
 import os
 
 import aws_cdk as cdk
@@ -6,12 +7,14 @@ import aws_cdk as cdk
 from infrastructure.vpc_stack import VPCStack
 from infrastructure.task_store_service_stack import TaskStoreServiceStack
 
+with open("cdk.json") as f:
+    cdk_config = json.load(f)
 
 app = cdk.App()
 pr_number = os.getenv('PR_NUMBER') if os.getenv('PR_NUMBER') else ""
-environment = app.node.try_get_context("environments.staging")
-account = environment["account"]
-region = environment["region"]
+environment = cdk_config.get("environments", {}).get("staging")
+account = environment.get("account")
+region = environment.get("region")
 env = cdk.Environment(account=account, region=region)
 
 if not pr_number:
